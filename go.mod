60d7b38c8549f41c9b91dc41abfd9a9a7371dc34
Jim Pick <jim@jimpick.com>
2022-07-24 22:07:01 -0700

    Add external module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jimpick/sp-kyc-go-test-simple
 
 go 1.18
+
+require github.com/google/go-cmp v0.5.8
--- hello.go
+++ hello.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/jimpick/sp-kyc-go-test-simple/morestrings"
 )
 
+// Tutorial: https://go.dev/doc/code
+
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
