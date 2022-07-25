6ec11ad9d308f806ae4fd2bb4fa6b315c019828b
Jim Pick <jim@jimpick.com>
2022-07-24 22:01:42 -0700

    New morestrings local module example

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jimpick/sp-kyc-go-test-simple/morestrings"
+)
 
 func main() {
-    fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
